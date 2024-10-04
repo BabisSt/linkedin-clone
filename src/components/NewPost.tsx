7d9705ddd997fc086b309f5e2818f793a6d31f97
BabisSt <mpamphsfe@gmail.com>
2024-10-04 20:20:08 +0300

    newPost changes

--- src/components/NewPost.tsx
+++ src/components/NewPost.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, ChangeEvent } from "react";
 import { Dialog, DialogBody } from "@material-tailwind/react";
 
 export default function Post() {
   const [openDial, setOpenDial] = useState(false);
-
+  const [postPhoto, setPostPhoto] = useState("");
+  const [postText, setPostText] = useState("");
+  const [postTitle, setPostTitle] = useState("");
   const handleOpenThoughts = () => {
     setOpenDial(!openDial);
   };
@@ -11,6 +13,22 @@ export default function Post() {
   const handleCloseThoughts = () => {
     setOpenDial(false);
   };
+
+  const handlePostTitle = (event: ChangeEvent<HTMLInputElement>) => {
+    setPostTitle(event.target.value);
+  };
+
+  const handlePostText = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    setPostText(event.target.value);
+  };
+
+  const handlePostPhoto = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files !== null)
+      setPostPhoto(URL.createObjectURL(event.target.files[0]));
+  };
+
+  const handlePost = () => {};
+
   return (
     <div className="flex py-8">
       <div className="flex shadow-lg rounded-lg mx-4 md:mx-auto max-w-lg md:max-w-4xl bg-blue-200 w-full">
@@ -19,7 +37,7 @@ export default function Post() {
             type="text"
             name="newPost"
             id="newPost"
-            className="bg-blue-200 border  text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+            className="bg-blue-200 border text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
             placeholder="Share your thoughts"
             onClick={handleOpenThoughts}
           />
@@ -27,99 +45,111 @@ export default function Post() {
         <Dialog
           open={openDial}
           handler={handleOpenThoughts}
-          className="flex items-center justify-center backdrop-blur-md fixed inset-0 z-50 bg-transparent"
+          className="flex justify-center backdrop-blur-md fixed inset-0 z-50 bg-transparent w-full "
           onClick={handleCloseThoughts}
           placeholder={undefined}
           onPointerEnterCapture={undefined}
           onPointerLeaveCapture={undefined}
         >
           <div
-            className="fixed inset-0 bg-black opacity-50 backdrop-blur-md"
+            className="fixed inset-0 bg-black opacity-50 backdrop-blur-md w-full"
             onClick={handleCloseThoughts}
           />
 
           <DialogBody
-            className=""
+            className="relative md:w-[40%] h-[60%] w-[100%]  flex flex-col" // Make DialogBody a flex container
             onClick={(e) => e.stopPropagation()}
             placeholder={undefined}
             onPointerEnterCapture={undefined}
             onPointerLeaveCapture={undefined}
           >
-            <div className="editor rounded-lg flex flex-col text-gray-800 border border-blue-200 p-4 shadow-lg max-w-2xl">
+            <div className="editor rounded-lg flex flex-col text-gray-800 border border-blue-200 p-4 shadow-lg w-full h-full overflow-auto">
               <input
-                className=" rounded-lg title bg-blue-200 border border-blue-200 p-2 mb-4 outline-none"
+                className="rounded-lg title bg-blue-200 border border-blue-200 p-2 mb-4 outline-none"
                 spellCheck="false"
                 placeholder="Title"
                 type="text"
+                onChange={handlePostTitle}
               />
               <textarea
-                className=" rounded-lg description bg-blue-200 sec p-3 h-60 border border-blue-200 outline-none"
+                className="rounded-lg description bg-blue-200 sec p-3 mb-2 h-60 border border-blue-200 outline-none"
                 spellCheck="false"
                 placeholder="What do you want to talk about?"
+                onChange={handlePostText}
               ></textarea>
 
-              <div className="icons flex text-gray-500 m-2">
-                <svg
-                  className="mr-2 cursor-pointer hover:text-gray-700 border rounded-full p-1 h-7"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+              {postPhoto && (
+                <div className=" relative w-full max-w-[100%]">
+                  <img
+                    src={postPhoto}
+                    className="rounded-lg w-full h-auto object-cover max-h-60 border border-blue-200 "
                   />
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
-                  />
-                </svg>
-                <svg
-                  className="mr-2 cursor-pointer hover:text-gray-700 border rounded-full p-1 h-7"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                  />
-                </svg>
-                <svg
-                  className="mr-2 cursor-pointer hover:text-gray-700 border rounded-full p-1 h-7"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"
-                  />
-                </svg>
-              </div>
+                  <button
+                    onClick={() => setPostPhoto("")}
+                    className="absolute top-2 right-2 p-1"
+                  >
+                    <svg
+                      className="mr-2 cursor-pointer hover:text-gray-700 border rounded-full p-1 h-7"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  </button>
+                </div>
+              )}
 
-              <div className="buttons flex">
-                <button
-                  onClick={handleCloseThoughts}
-                  className="btn border rounded-lg border-blue-200 p-1 px-4 font-semibold cursor-pointer text-gray-500 ml-auto"
+              <div className="icons flex text-gray-500 m-2">
+                <label
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white cursor-pointer"
+                  htmlFor="file_input"
                 >
-                  Close
-                </button>
-                <button className="p-1 px-4 ml-2 font-semibold bg-gradient-to-r from-cyan-500 to-blue-500 rounded-lg text-center transition-transform transform">
-                  Post
-                </button>
+                  <svg
+                    className="mr-2 cursor-pointer hover:text-gray-700 border rounded-full p-1 h-7"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"
+                    />
+                  </svg>
+                </label>
+
+                <input
+                  className="hidden"
+                  id="file_input"
+                  type="file"
+                  accept=".png,.jpg,.jpeg,.gif"
+                  onChange={handlePostPhoto}
+                />
               </div>
+            </div>
+
+            <div className="absolute bottom-0 left-0 w-full flex justify-end p-7 ">
+              <button
+                onClick={handleCloseThoughts}
+                className="btn border rounded-lg border-blue-200 p-1 px-4 font-semibold cursor-pointer text-gray-500"
+              >
+                Close
+              </button>
+              <button
+                onSubmit={handlePost}
+                className="p-1 px-4 ml-2 font-semibold bg-gradient-to-r from-cyan-500 to-blue-500 rounded-lg text-center transition-transform transform"
+              >
+                Post
+              </button>
             </div>
           </DialogBody>
         </Dialog>
